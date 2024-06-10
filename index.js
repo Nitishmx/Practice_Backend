029846c2cb2d045618535f919d098ddcc0e8ab42
nitishmx <nitish7739879577@gmail.com>
2024-06-10 21:49:47 +0530

    other file create schema and import other file

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ app.get("/", (req, res) => {
 });
 
 // mongodb connect with express js (server)
-let mongoose = require("mongoose");
-mongoose
-  .connect("mongodb://127.0.0.1:27017/newdatabasecreate")
-  .then(() => {
-    console.log("database easily connect");
-  })
-  .catch(() => {
-    console.log("some error");
-  });
+// let mongoose = require("mongoose");
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/newdatabasecreate")
+//   .then(() => {
+//     console.log("database easily connect");
+//   })
+//   .catch(() => {
+//     console.log("some error");
+//   });
 // database connection done
 
 
@@ -22,24 +22,37 @@ mongoose
 
 
 // schemacreate
-let Userschema = mongoose.Schema({
-  name: String,
-  lastName: String,
-  num: Number,
-  isBool: Boolean,
-});
+// let Userschema = mongoose.Schema({
+//   name: String,
+//   lastName: String,
+//   num: Number,
+//   isBool: Boolean,
+// });
 
 // collection create
-let User=mongoose.model("User",Userschema)
+// let User=mongoose.model("User",Userschema)
 // collection create done
 
 
 // collection inner data add
-let user=new User({name:"hye",last:"bye",num:1234,isBool:true})
-user.save()
-let n=new User({name:"good",lastName:"thank you",num:7878,isBool:false})
-n.save()
+// let user=new User({name:"hye",last:"bye",num:1234,isBool:true})
+// user.save()
+// let n=new User({name:"good",lastName:"thank you",num:7878,isBool:false})
+// n.save()
 // data addition done
+
+
+// schema and collection create otherfile and import other file
+let Good=require('./mongodb')
+let mongoose=require('mongoose')
+mongoose.connect("mongodb://127.0.0.1:27017/other").then(()=>{
+    console.log("database connection successful connect");
+}).catch(()=>{
+    console.log("some error create to connect database");
+})
+
+let nn=new Good({subject:"programming",mark:98,totalMark:99,passStatus:true})
+nn.save()
 
 app.listen(4000, () => {
   console.log("server runing");
--- mongodb.js
+++ mongodb.js
@@ -1,1 +1,15 @@
+const { name } = require('ejs')
 let mongoose=require('mongoose')
+let userSchema=mongoose.Schema({
+    subject:String,
+    mark:Number,
+    totalMarks:Number,
+    passStatus:Boolean,
+    result:Boolean
+})
+
+let Good=mongoose.model('Good',userSchema)
+
+
+// backend anythink export to use module
+module.exports=Good
