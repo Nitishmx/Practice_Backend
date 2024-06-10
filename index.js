8d99ad780669476691a68e580ae717db4901a514
nitishmx <nitish7739879577@gmail.com>
2024-06-10 19:46:00 +0530

    schema and collection create

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 console.log("database connection");
-let express=require('express')
-let app=express()
-app.get('/',(req,res)=>{
-    res.send("home route create")
-})
-
+const { name } = require("ejs");
+let express = require("express");
+let app = express();
+app.get("/", (req, res) => {
+  res.send("home route create");
+});
 
 // mongodb connect with express js (server)
-let mongoose=require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/newdatabasecreate").then(()=>{
+let mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/newdatabasecreate")
+  .then(() => {
     console.log("database easily connect");
-}).catch(()=>{
+  })
+  .catch(() => {
     console.log("some error");
-})
+  });
 // database connection done
 
+// schemacreate
 
+let Userschema = mongoose.Schema({
+  name: String,
+  lastName: String,
+  num: Number,
+  isBool: Boolean,
+});
 
+// collection create
+let User=mongoose.model("User",Userschema)
+// collection create done
 
-app.listen(4000,()=>{
-    console.log("server runing");
-})
+app.listen(4000, () => {
+  console.log("server runing");
+});
